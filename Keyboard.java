4e4a749c1fac2f1cb4f0a4f8e23c5a2f4e741d7f
OepenProPra2013 <Jan-Hendrik.Oepen@uni-duesseldorf.de>
2013-05-20 19:49:13 +0200

    Create Keyboard.java

---
+++ Keyboard.java
@@ -0,0 +1,37 @@
+package MoveTest;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Keyboard implements KeyListener {
+  private static boolean[] keys = new boolean[1024]; 
+	
+	public static boolean isKeyDown (int keyCode){
+		if(keyCode>=0&&keyCode<keys.length) return keys[keyCode];
+		else  return false;
+	}
+	
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		int keyCode = e.getKeyCode();
+		if(keyCode>=0&&keyCode<keys.length) keys[keyCode]=true; 
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		int keyCode = e.getKeyCode();
+		if(keyCode>=0&&keyCode<keys.length) keys[keyCode]=false;
+	}
+	
+	
+	
+	
+	
+	
+    //Unnötig 
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+	}
+
+}
